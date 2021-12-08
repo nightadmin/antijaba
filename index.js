c13a31064bd6c2b61b88fc8de048b834dc8ab59a
Mark Fomin <luring.uliksir@google.com>
2021-12-08 23:47:38 +0300

    some featured added

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+if (localStorage.getItem("auth") == null) {
+  var auth = false;
+}
+else {
+  var auth = localStorage.getItem("name");
+}
+if (auth) {
+  document.querySelector("div.docs").style.display = "none";
+  document.querySelector("div.hello").innerHTML = `Привет, ${auth}!`;
+}
+function _auth(token, name) {
+  localStorage.setItem("auth", true);
+  localStorage.setItem("name", name);
+  localStorage.setItem("token", token);
+}
